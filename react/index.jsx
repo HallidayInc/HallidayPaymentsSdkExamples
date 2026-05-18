05785df7e2b635a152b69b1d98c152a1dbbb06e2
Adam Bavosa <adam@halliday.xyz>
2026-05-18 19:47:06 -0400

    updaets for 3.1.0

--- react/index.jsx
+++ react/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { openHallidayPayments } from '@halliday-sdk/payments';
+import { openHallidayPayments, initializeClient } from '@halliday-sdk/payments';
+import { connectSigner } from '@halliday-sdk/payments/ethers';
+import { BrowserProvider } from 'ethers';
 
 const HALLIDAY_API_KEY = '_your_api_key_here_';
 
@@ -8,20 +11,33 @@ if (HALLIDAY_API_KEY === '_your_api_key_here_') {
   alert('HALLIDAY_API_KEY is missing!');
 }
 
+const hallidayOutputs = ['base:0x833589fcd6edb6e08f4c7c32d4f71b54bda02913'];
+
 function App() {
+  useEffect(() => {
+    initializeClient({
+      apiKey: HALLIDAY_API_KEY,
+      outputs: hallidayOutputs,
+    });
+  }, []);
+
   return (
     <div>
       <button
-        onClick={() => {
+        onClick={async () => {
+          const _ethereum = window.ethereum || window.phantom.ethereum;
+          const [address] = await _ethereum.request({ method: "eth_requestAccounts" });
+
+          const userWallet = connectSigner(() =>
+            new BrowserProvider(_ethereum).getSigner(),
+          );
+
           openHallidayPayments({
             apiKey: HALLIDAY_API_KEY,
-            services: ['SWAP'],
             // USDC on Base
-            inputs: ['base:0x833589fcd6edb6e08f4c7c32d4f71b54bda02913'],
-            // IP on Story
-            outputs: ['story:0x'],
-            sandbox: false,
-            windowType: 'POPUP',
+            outputs: hallidayOutputs,
+            userWallet,
+            destinationAddress: address,
           });
       }}>
         Open Halliday
